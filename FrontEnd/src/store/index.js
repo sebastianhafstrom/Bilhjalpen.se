025ef1dbd97d022d94bca9afbef3dbebb9cac220
Sebastian Hafström <hafstrom.sebastian@gamil.com>
2020-08-26 11:34:06 +0200

    Introduced the vuex store and added states

--- FrontEnd/src/store/index.js
+++ FrontEnd/src/store/index.js
@@ -5,8 +5,31 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    body: [],
+    fuel: [],
+    sorting: ''
   },
   mutations: {
+    setBody(state, selection) {
+      state.body = selection
+    },
+    setFuel(state, selection) {
+      state.fuel = selection
+    },
+    setSort(state, selection) {
+      state.sorting = selection
+    }
+  },
+  getters: {
+    getFilteredFuels: state => {
+      return state.fuel
+    },
+    getFilteredBodies: state => {
+      return state.body
+    },
+    getSort: state => {
+      return state.sorting
+    }
   },
   actions: {
   },
