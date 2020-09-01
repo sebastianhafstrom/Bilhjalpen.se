0397d185f8bca870891590e79fe96ad8dfde7fa0
Sebastian Hafström <hafstrom.sebastian@gmail.com>
2020-09-02 00:13:54 +0200

    Took back the navbar, but gotta fix screen breakpoints for hamburger menu

--- FrontEnd/src/views/Navbar.vue
+++ FrontEnd/src/views/Navbar.vue
@@ -1,9 +1,39 @@
 <template>
   <nav>
-    <v-app-bar color="red accent-4" dark>
-      <v-toolbar-title class="headline text-uppercase">CarPicker</v-toolbar-title>
+    <v-app-bar color="primary" dark height="80">
+      <router-link to="/"><img class="mr-3" :src="require('../assets/logo.png')" height="40" to="/" /></router-link>
+      <!--<v-toolbar-title class="headline">Bilhjälpen</v-toolbar-title> -->
       <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn text to="/cars" exact>Alla bilar</v-btn>
+      </v-toolbar-items>
+      <v-app-bar-nav-icon class="hidden-sm-and-up" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
     </v-app-bar>
+
+    <!-- Start of mobile side menu -->
+    <v-navigation-drawer app v-model="drawer" right disable-resize-watcher>
+      <!-- Menu title -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">Meny</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+      <!-- Menu Links -->
+      <v-list>
+        <v-list-item v-for="item in items" :key="item.title" :to="item.link">
+          <!-- <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>-->
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <!-- End of mobile side menu -->
   </nav>
 </template>
 
@@ -11,6 +41,11 @@
 export default {
   name: "Navbar",
   data: () => ({
-  })
+    drawer: false, // Hide mobile side menu by default
+    items: [
+      { text: "Hem", link: "/" },
+      { text: "Alla bilar", link: "/cars" },
+    ],
+  }),
 };
 </script>
