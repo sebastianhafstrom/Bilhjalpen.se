3b5a5b53f8cf1972ec4aea95026dc6551d02c239
Sebastian Hafström <hafstrom.sebastian@gmail.com>
2020-09-03 09:08:25 +0200

    Added use of environment variables

--- FrontEnd/src/Api.js
+++ FrontEnd/src/Api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: process.env.VUE_APP_API_ENDPOINT || 'http://localhost:3000/api'
+  baseURL: process.env.BACKEND_URL || 'http://localhost:3000/api'
 })
