02a94f417d0c5f5f542302d8002665297f5e302a
Sebastian Hafström <hafstrom.sebastian@gmail.com>
2020-09-07 11:13:38 +0200

    some small changes to About

--- FrontEnd/src/views/About.vue
+++ FrontEnd/src/views/About.vue
@@ -1,10 +1,6 @@
 <template>
     <v-container>
-    <v-row
-        
-        align="left"
-        justify="left"
-        >
+    <v-row>
         <v-col class="text-left" cols="12">
             <h1 class="display-1 font-weight-thin mb-4">Om Bilhjälpen.se</h1>
             <p>På Bilhjälpen.se vill vi hjälpa vanliga konsumenter göra bättre bilköp. Genom att erbjuda möjligheten att i lugn och ro kunna göra oberoende jämförelser mellan mängder av olika bilmärken, modeller och varianter så kan konsumneten hitta de bilar som passar just den. Även om man inte är intresserade av långa rader av specifikation så kan kan Bilhjälpen.se hälpa just dig. Vi erbjuder ett verktyg där du beskriver dina begov och din livssituation. Vi rekommenderar bilar beroende på dina svar och ser till att du får en variation av bilar att välja mellan.</p>
@@ -17,7 +13,6 @@
 <script>
 export default {
     name: "About",
-
 }
 </script>
 
