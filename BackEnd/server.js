6b9b554f10f292db7cdc5e214fe419d96b705b07
Sebastian Hafström <hafstrom.sebastian@gamil.com>
2020-09-17 17:31:17 +0200

    Introducing Backend Auth with JWT

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,15 +1,16 @@
 var express = require('express')
 require('dotenv').config()
+
 var brandController = require('./controllers/brandcontroller')
 var modelController = require('./controllers/modelcontroller')
 var specsController = require('./controllers/specscontroller')
 var categoryController = require('./controllers/categorycontroller')
 
+var authorize = require('./Middlewares/auth.js')
 const bodyParser= require('body-parser')
-const port = process.env.PORT || 3000
+
 const app = express()
 var cors = require('cors')
-
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -17,17 +18,21 @@ app.use(express.static('public'))
 app.options('*', cors())
 app.use(cors())
 
-app.get('/', function(req, res) {
-    res.send("<h1>Welcome to the CarPicker API</h1>");
-})
-app.get('/api', function(req, res) {
-    res.send("<h1>Welcome to the CarPicker API</h1>");
-})
-
+//Middlewear for verifying JWT
+app.use(authorize.authorize)
 app.use('/api/brands', brandController.router)
 app.use('/api/models', modelController.router)
 app.use('/api/specs', specsController.router)
 app.use('/api/category', categoryController.router)
+
+app.get('/', function(req, res) {
+    res.send("<h1>Welcome to the CarPicker API</h1>");
+})
+app.get('*', function(req, res) {
+    res.send("<h1>Welcome to the CarPicker API</h1>");
+})
+
+
 
 
 app.use((req, res, next) => {
@@ -47,10 +52,10 @@ app.use((error, req, res) => {
 
 var env = app.get('env')
 
-app.listen(port, function(err) {
+app.listen(process.env.PORT, function(err) {
     if (err) throw err
-    console.log(`Express server listening on port ${port}, in ${env} mode`)
-    console.log(`Backend: http://localhost:${port}/`)
+    console.log(`Express server listening on port ${process.env.PORT}, in ${env} mode`)
+    console.log(`Backend: http://localhost:${process.env.PORT}/`)
 })
 
 
